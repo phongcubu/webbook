194f84f5d858db481525b4dee26621e90c8edb6c
phongcubu <thanhphongdz2707@gmail.com>
2021-09-10 08:31:38 +0700

    them asset vao duong dan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,18 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Home | E-Shopper</title>
-    <link href="{{('public/frontend/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/price-range.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/animate.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/main.css')}}" rel="stylesheet">
-    <link href="{{('public/frontend/css/responsive.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontend/css/responsive.css')}}" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="{{('public/frontend/images/ico/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{asset('public/frontend/images/ico/favicon.ico')}}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
@@ -57,7 +57,7 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="logo pull-left">
-                            <a href="index.html"><img src="{{('public/frontend/images/home/logo.png')}}" alt="" /></a>
+                            <a href="index.html"><img src="{{asset('public/frontend/images/home/logo.png')}}" alt="" /></a>
                         </div>
                         <div class="btn-group pull-right">
                             <div class="btn-group">
@@ -157,8 +157,8 @@
                                     <button type="button" class="btn btn-default get">Get it now</button>
                                 </div>
                                 <div class="col-sm-6">
-                                    <img src="{{('public/frontend/images/home/girl1.jpg')}}" class="girl img-responsive" alt="" />
-                                    <img src="{{('public/frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/girl1.jpg')}}" class="girl img-responsive" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
                                 </div>
                             </div>
                             <div class="item">
@@ -169,8 +169,8 @@
                                     <button type="button" class="btn btn-default get">Get it now</button>
                                 </div>
                                 <div class="col-sm-6">
-                                    <img src="{{('public/frontend/images/home/girl2.jpg')}}" class="girl img-responsive" alt="" />
-                                    <img src="{{('public/frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/girl2.jpg')}}" class="girl img-responsive" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/pricing.png')}}"  class="pricing" alt="" />
                                 </div>
                             </div>
                             
@@ -182,8 +182,8 @@
                                     <button type="button" class="btn btn-default get">Get it now</button>
                                 </div>
                                 <div class="col-sm-6">
-                                    <img src="{{('public/frontend/images/home/girl3.jpg')}}" class="girl img-responsive" alt="" />
-                                    <img src="{{('public/frontend/images/home/pricing.png')}}" class="pricing" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/girl3.jpg')}}" class="girl img-responsive" alt="" />
+                                    <img src="{{asset('public/frontend/images/home/pricing.png')}}" class="pricing" alt="" />
                                 </div>
                             </div>
                             
@@ -255,7 +255,7 @@
                             <div class="video-gallery text-center">
                                 <a href="#">
                                     <div class="iframe-img">
-                                        <img src="{{('public/frontend/images/home/iframe1.png')}}" alt="" />
+                                        <img src="{{asset('public/frontend/images/home/iframe1.png')}}" alt="" />
                                     </div>
                                     <div class="overlay-icon">
                                         <i class="fa fa-play-circle-o"></i>
@@ -270,7 +270,7 @@
                             <div class="video-gallery text-center">
                                 <a href="#">
                                     <div class="iframe-img">
-                                        <img src="{{('public/frontend/images/home/iframe2.png')}}" alt="" />
+                                        <img src="{{asset('public/frontend/images/home/iframe2.png')}}" alt="" />
                                     </div>
                                     <div class="overlay-icon">
                                         <i class="fa fa-play-circle-o"></i>
@@ -285,7 +285,7 @@
                             <div class="video-gallery text-center">
                                 <a href="#">
                                     <div class="iframe-img">
-                                        <img src="{{('public/frontend/images/home/iframe3.png')}}" alt="" />
+                                        <img src="{{asset('public/frontend/images/home/iframe3.png')}}" alt="" />
                                     </div>
                                     <div class="overlay-icon">
                                         <i class="fa fa-play-circle-o"></i>
@@ -300,7 +300,7 @@
                             <div class="video-gallery text-center">
                                 <a href="#">
                                     <div class="iframe-img">
-                                        <img src="{{('public/frontend/images/home/iframe4.png')}}" alt="" />
+                                        <img src="{{asset('public/frontend/images/home/iframe4.png')}}" alt="" />
                                     </div>
                                     <div class="overlay-icon">
                                         <i class="fa fa-play-circle-o"></i>
@@ -313,7 +313,7 @@
                     </div>
                     <div class="col-sm-3">
                         <div class="address">
-                            <img src="{{('public/frontend/images/home/map.png')}}" alt="" />
+                            <img src="{{asset('public/frontend/images/home/map.png')}}" alt="" />
                             <p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
                         </div>
                     </div>
@@ -400,11 +400,11 @@
     
 
   
-    <script src="{{('public/frontend/js/jquery.js')}} "></script>
-    <script src="{{('public/frontend/js/bootstrap.min.js')}} "></script>
-    <script src="{{('public/frontend/js/jquery.scrollUp.min.js')}} "></script>
-    <script src="{{('public/frontend/js/price-range.js')}} "></script>
-    <script src="{{('public/frontend/js/jquery.prettyPhoto.js')}} "></script>
-    <script src="{{('public/frontend/js/main.js')}} "></script>
+    <script src="{{asset('public/frontend/js/jquery.js')}} "></script>
+    <script src="{{asset('public/frontend/js/bootstrap.min.js')}} "></script>
+    <script src="{{asset('public/frontend/js/jquery.scrollUp.min.js')}} "></script>
+    <script src="{{asset('public/frontend/js/price-range.js')}} "></script>
+    <script src="{{asset('public/frontend/js/jquery.prettyPhoto.js')}} "></script>
+    <script src="{{asset('public/frontend/js/main.js')}} "></script>
 </body>
 </html>
