d2314d8e62752774c148942dbe50cb16664de612
phuong152vs <phuong10a2vs1522001@gmail.com>
2021-09-28 16:34:33 +0700

    css

--- resources/views/admin/manage_order.blade.php
+++ resources/views/admin/manage_order.blade.php
@@ -12,27 +12,7 @@
           Session::put("message",null);
       }
   ?>
-      {{-- <div class="row w3-res-tb">
-        <div class="col-sm-5 m-b-xs">
-          <select class="input-sm form-control w-sm inline v-middle">
-            <option value="0">Bulk action</option>
-            <option value="1">Delete selected</option>
-            <option value="2">Bulk edit</option>
-            <option value="3">Export</option>
-          </select>
-          <button class="btn btn-sm btn-default">Apply</button>                
-        </div>
-        <div class="col-sm-4">
-        </div>
-        <div class="col-sm-3">
-          <div class="input-group">
-            <input type="text" class="input-sm form-control" placeholder="Search">
-            <span class="input-group-btn">
-              <button class="btn btn-sm btn-default" type="button">Go!</button>
-            </span>
-          </div>
-        </div>
-      </div> --}}
+      
       <div class="table-responsive">
         <table class="table table-striped b-t b-light">
           <thead>
@@ -45,9 +25,10 @@
               <th>Tên người đặt</th>
               <th>Tổng giá tiền</th>
               <th>Tình trạng</th>
-              <th>Hiển Thị</th>
-            
+             
               <th style="width:30px;"></th>
+              <th>Chi tiết đơn </th>
+            
             </tr>
           </thead>
           <tbody>
@@ -61,9 +42,7 @@
                     <a href="{{URL::to('view-order/'.$order->order_id)}}" class="active styling-edit" ui-toggle-class="">
                   <i class="fa fa-pencil-square-o text-success text-active"></i>
                 </a>
-                <a  onclick="return confirm('bạn chắc chắn muốn xóa đơn hàng không ?')" href="{{URL::to('delete-order/'.$order->order_id)}}" class="active styling-edit" ui-toggle-class="">
-                  <i class="fa fa-times text-danger text"></i>
-                </a>
+               
               </td>
             </tr>
             @endforeach
--- resources/views/pages/sanpham/show_details.blade.php
+++ resources/views/pages/sanpham/show_details.blade.php
@@ -71,14 +71,14 @@
                     
                     <div class="category-tab shop-details-tab"><!--category-tab-->
 						<div class="col-sm-12">
-							<ul class="nav nav-tabs">
+							<ul class="nav nav-tabs">	
 								<li class="active"><a href="#details" data-toggle="tab">Mô tả sản phẩm</a></li>
 								<li><a href="#companyprofile" data-toggle="tab">Chi tiết sản phẩm</a></li>
 								{{-- <li><a href="#reviews" data-toggle="tab">Đánh giá</a></li> --}}
 							</ul>
 						</div>
 						<div class="tab-content">
-							<div class="tab-pane fade active in" id="details" >
+							<div class="tab-pane fade active in" id="details"  >
                                 <p>{!!$value->product_desc!!}</p>
 							</div>
 							<div class="tab-pane fade" id="companyprofile" >
